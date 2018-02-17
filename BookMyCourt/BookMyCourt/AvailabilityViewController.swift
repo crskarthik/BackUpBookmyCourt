6ae402462cc3e3b3cf823fa1894fbf56bac0e3a5
student <s530458@ch112010m.nwmissouri.edu>
2018-02-16 19:35:59 -0600

    Ashok: Added availability view

--- BookMyCourt/BookMyCourt/AvailabilityViewController.swift
+++ BookMyCourt/BookMyCourt/AvailabilityViewController.swift
@@ -8,18 +8,30 @@
 
 import UIKit
 
-class AvailabilityViewController: UIViewController {
-
+class AvailabilityViewController: UIViewController,UITableViewDataSource,UITableViewDelegate {
+    let days = ["DAYS","Monday","Tuesday","Wednesday","Thrusday","Friday","Saturday","Sunday"]
+    let availablity = ["STATUS","Available","Not Available","Available","Not Available","Available","Not Available","Available"]
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return days.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "availabilityList")!
+        cell.textLabel?.text = days[indexPath.row]
+        cell.detailTextLabel?.text = availablity[indexPath.row]
+        return cell
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
 
 }
 
