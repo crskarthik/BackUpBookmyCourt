df9aa4e8c4600f7df5fe29a64e7174ea833e6d3f
student <student@nwmsu.local>
2018-03-09 22:15:03 -0600

    Ashok - Added models user and booking

--- BookMyCourt/BookMyCourt/NewBookingViewController.swift
+++ BookMyCourt/BookMyCourt/NewBookingViewController.swift
@@ -10,6 +10,10 @@ import UIKit
 
 class NewBookingViewController: UIViewController {
 
+    @IBOutlet weak var Txt919Number: UITextField!
+    @IBOutlet weak var TxtPhoneNumber: UITextField!
+    @IBOutlet weak var LblMessage: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,7 +23,17 @@ class NewBookingViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
+    override func shouldPerformSegue(withIdentifier identifier: String, sender: Any?) -> Bool {
+        if Txt919Number.text! == "919558155" && Txt919Number.text != nil {
+            return true
+        }
+        else{
+            LblMessage.text = "Invalid 919 Number"
+            return false
+        }
+    }
+    
 
 }
 
